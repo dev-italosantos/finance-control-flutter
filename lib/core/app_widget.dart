580decb01da7b289afc32ecb8961fd4bc5e9a337
dev-italosantos <italotayllor@gmail.com>
2023-01-05 23:49:06 -0300

    feat: add theme in app widget

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black12,
+        canvasColor: Colors.black26,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white)
+            .copyWith(
+          bodyText1: const TextStyle(color: Colors.blueAccent),
+          bodyText2: const TextStyle(color: Colors.blueAccent),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Portfólio Dev Italo Santos',
+      home: SplashPage(),
+    );;
   }
 }
