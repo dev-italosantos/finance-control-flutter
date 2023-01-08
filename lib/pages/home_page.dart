6a6b0119dcd389cd994229d85e5a52ffa8260f8c
dev-italosantos <italotayllor@gmail.com>
2023-01-07 23:57:20 -0300

    feat: b3 api integration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  Future<String> getData() async {
+    var url = Uri.https('https://api.b3.com.br/b3api/api/v1/ativos', '');
+    var response = await http.get(url);
+
+    return response.body;
+  }
+
+  void mainTest() async {
+    var data = await getData();
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
+    mainTest();
+
     return Container();
   }
 }
