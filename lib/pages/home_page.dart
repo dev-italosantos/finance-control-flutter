867a254baae9e77e9a60e75d77bbcca2c4e80e85
dev-italosantos <italotayllor@gmail.com>
2023-01-20 23:02:56 -0300

    feat: add dependency font_awesome_flutter #

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,38 @@
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  Future<String> getData() async {
-    var url = Uri.https('https://api.b3.com.br/b3api/api/v1/ativos', '');
-    var response = await http.get(url);
+  // Future<String> getData() async {
+  //   var url = Uri.https('https://api.b3.com.br/b3api/api/v1/ativos', '');
+  //   var response = await http.get(url);
+  //
+  //   return response.body;
+  // }
+  //
+  // void mainTest() async {
+  //   var data = await getData();
+  //   print(data);
+  // }
 
-    return response.body;
-  }
-
-  void mainTest() async {
-    var data = await getData();
-    print(data);
+  Widget _bottomAction(IconData icon) {
+    return InkWell(
+      child: Icon(icon),
+      onTap: () {},
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    mainTest();
-
-    return Container();
+    return Scaffold(
+       bottomNavigationBar: BottomAppBar(
+         child: Row(
+           children: [
+             _bottomAction()
+           ],
+         ),
+       ),
+    );
   }
 }
