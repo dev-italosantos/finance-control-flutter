e78f43df2d6770d9d51bad7c527bac88e1237daa
dev-italosantos <italotayllor@gmail.com>
2023-01-24 22:18:03 -0300

    feat: Add LineChart widget

--- lib/widgets/graph_widget.dart
+++ lib/widgets/graph_widget.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:charts_flutter/flutter.dart';
 
 import 'package:flutter/material.dart';
 
@@ -20,8 +21,44 @@ class _GraphWidgetState extends State<GraphWidget> {
     data = List<double>.generate(50, (index) => r.nextDouble() * 1500);
   }
 
+  _onSelectionChanged(SelectionModel model) {
+    final selectedDatum = model.selectedDatum;
+
+    var time;
+    final measures = <String, double>{};
+
+    if (selectedDatum.isNotEmpty) {
+      time = selectedDatum.first.datum;
+      selectedDatum.forEach((SeriesDatum datumPair) {
+        measures[datumPair.series.displayName] = datumPair.datum;
+      });
+    }
+
+    print(time);
+    print(measures);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<Series<double, num>> series = [
+      Series<double, int>(
+        id: '63d07b59b1159',
+        data: data,
+        colorFn: (_, __) => MaterialPalette.blue.shadeDefault,
+        domainFn: (value, index) => index,
+        measureFn: (value, _) => value,
+        strokeWidthPxFn: (_, __) => 4
+      )
+    ];
+    return LineChart(
+      series,
+      animate: false,
+      selectionModels: [
+        SelectionModelConfig(
+          type: SelectionModelType.info,
+          changedListener: _onSelectionChanged
+        )
+      ],
+    );
   }
 }
