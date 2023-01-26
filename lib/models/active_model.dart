9440571e77ddfe9d953cb7a9b557963cfa78b08e
dev-italosantos <italotayllor@gmail.com>
2023-01-26 19:36:49 -0300

    feat: add active model

---
+++ lib/models/active_model.dart
@@ -0,0 +1,14 @@
+class Active {
+  String icon;
+  String name;
+  String acronym;
+  double price;
+
+  Active({
+    required this.icon,
+    required this.name,
+    required this.acronym,
+    required this.price
+  });
+
+}
