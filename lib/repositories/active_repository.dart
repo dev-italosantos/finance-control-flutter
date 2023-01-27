7f17f2cebd3c57c4b7965aae07e17f26093fa07d
dev-italosantos <italotayllor@gmail.com>
2023-01-26 22:38:23 -0300

    feat: add asset data

---
+++ lib/repositories/active_repository.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_investment_control/models/active_model.dart';
+import '../core/app_icons.dart';
+
+class ActiveRepository {
+  static List<Active> tabela = [
+    Active(
+      icon: AppIcons.btc,
+      name: 'Bitcoin',
+      acronym: 'BTC',
+      price: 22.924,
+    ),
+    Active(
+      icon: AppIcons.btc,
+      name: 'Bitcoin',
+      acronym: 'BTC',
+      price: 22.924,
+    ),
+    Active(
+      icon: AppIcons.btc,
+      name: 'Bitcoin',
+      acronym: 'BTC',
+      price: 22.924,
+    ),
+    Active(
+      icon: AppIcons.btc,
+      name: 'Bitcoin',
+      acronym: 'BTC',
+      price: 22.924,
+    ),
+    Active(
+      icon: AppIcons.btc,
+      name: 'Bitcoin',
+      acronym: 'BTC',
+      price: 22.924,
+    )
+  ];
+}
