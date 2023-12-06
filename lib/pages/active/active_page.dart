cf1a7b8d57a78cba38f5d2a6031cb1859430d410
dev-italosantos <italotayllor@gmail.com>
2023-12-06 02:30:54 -0300

    refactor: update to brapi API

--- lib/pages/active/active_page.dart
+++ lib/pages/active/active_page.dart
@@ -288,38 +288,39 @@ class _AssetListState extends State<AssetList> {
 
 
   Future<Map<String, dynamic>?> getAssetDetails(String ticker) async {
-    final apiUrl =
-        'https://financialmodelingprep.com/api/v3/profile/$ticker?apikey=0lkDQ0T5fKjLrjhYZuWZKENjIbogLCxU';
+    final apiUrl = 'https://brapi.dev/api/quote/$ticker?token=m2VDSqSjN5diYAp5VjZSNv';
 
     try {
       final response = await http.get(Uri.parse(apiUrl));
 
       if (response.statusCode == 200) {
         final jsonData = jsonDecode(response.body);
+        print('Resposta JSON: $jsonData');
 
-        // Verifica se a resposta contém dados e o detalhe do ativo
-        if (jsonData != null && jsonData.isNotEmpty) {
+        if (jsonData != null && jsonData['results'] != null && jsonData['results'].isNotEmpty) {
           final assetDetails = {
-            'currentPrice': jsonData[0]['price'],
+            'currentPrice': jsonData['results'][0]['regularMarketPrice'] ?? 0.0,
           };
 
           return assetDetails;
         } else {
-          print('Erro: Dados ausentes na resposta JSON');
+          print('Detalhes do ativo não encontrados para o ticker: $ticker');
+          // Adicione lógica para lidar com a ausência de detalhes do ativo
+          return null;
         }
       } else {
-        print(
-            'Falha ao obter detalhes do ativo. Status: ${response.statusCode}');
+        print('Falha ao obter detalhes do ativo. Status: ${response.statusCode}');
+        // Adicione lógica para lidar com falhas na resposta do servidor
+        return null;
       }
     } catch (error) {
       print('Erro ao obter detalhes do ativo: $error');
-      // Adicione esta linha para propagar a exceção e ver detalhes no console
+      // Adicione lógica para lidar com exceções durante a solicitação HTTP
       throw error;
     }
-
-    // Em caso de falha, retorne null ou trate de acordo com sua lógica
-    return null;
   }
+
+
 
   Future<void> _saveAssets() async {
     final prefs = await SharedPreferences.getInstance();
