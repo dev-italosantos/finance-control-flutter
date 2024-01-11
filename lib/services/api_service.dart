e79bed461da141352d58e2081b19e94df6718bf6
dev-italosantos <italotayllor@gmail.com>
2024-01-11 15:53:42 -0300

    refactor: get the asset details from the first API. If the first API returns no results, it tries to get the details from the second API

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,33 +3,50 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   Future<Map<String, dynamic>?> getAssetDetails(String ticker) async {
-    final apiUrl = 'https://brapi.dev/api/quote/$ticker?token=m2VDSqSjN5diYAp5VjZSNv';
+    final apiUrl1 = 'https://brapi.dev/api/quote/$ticker?token=m2VDSqSjN5diYAp5VjZSNv';
+    final apiUrl2 = 'https://mfinance.com.br/api/v1/stocks?symbols=$ticker';
 
     try {
-      final response = await http.get(Uri.parse(apiUrl));
+      // Tentar obter detalhes do ativo da primeira API
+      final response1 = await http.get(Uri.parse(apiUrl1));
 
-      if (response.statusCode == 200) {
-        final jsonData = jsonDecode(response.body);
-        print('Resposta JSON: $jsonData');
+      if (response1.statusCode == 200) {
+        final jsonData1 = jsonDecode(response1.body);
 
-        if (jsonData != null &&
-            jsonData['results'] != null &&
-            jsonData['results'].isNotEmpty) {
-          final assetDetails = {
-            'currentPrice': jsonData['results'][0]['regularMarketPrice'] ?? 0.0,
+        if (jsonData1 != null) {
+          final assetDetails1 = {
+            'currentPrice': jsonData1['results'][0]['regularMarketPrice'] ?? 0.0,
+            // Adicione outros campos conforme necessário
           };
 
-          return assetDetails;
-        } else {
-          print('Detalhes do ativo não encontrados para o ticker: $ticker');
-          // Adicione lógica para lidar com a ausência de detalhes do ativo
-          return null;
+          return assetDetails1;
         }
-      } else {
-        print('Falha ao obter detalhes do ativo. Status: ${response.statusCode}');
-        // Adicione lógica para lidar com falhas na resposta do servidor
-        return null;
       }
+
+      // Se não houver resultados da primeira API, tentar obter da segunda API
+      final response2 = await http.get(Uri.parse(apiUrl2));
+
+      if (response2.statusCode == 200) {
+        final jsonData2 = jsonDecode(response2.body);
+
+        if (jsonData2 != null &&
+            jsonData2['stocks'] != null &&
+            jsonData2['stocks'] is List &&
+            jsonData2['stocks'].isNotEmpty) {
+          final stockDetails = jsonData2['stocks'][0];
+          final assetDetails2 = {
+            'currentPrice': stockDetails['lastPrice'] ?? 0.0,
+            'name': stockDetails['name'] ?? '',
+            'segment': stockDetails['segment'] ?? ''
+            // Adicione outros campos conforme necessário
+          };
+
+          return assetDetails2;
+        }
+      }
+
+      // Se não houver resultados de ambas as APIs, retornar null
+      return null;
     } catch (error) {
       print('Erro ao obter detalhes do ativo: $error');
       // Adicione lógica para lidar com exceções durante a solicitação HTTP
