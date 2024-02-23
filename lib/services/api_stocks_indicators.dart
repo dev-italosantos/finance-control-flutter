56779d35dc56067cf2840923889fa5f91ca02934
dev-italosantos <italotayllor@gmail.com>
2024-02-22 21:45:24 -0300

    refactor: refactoring api mfinance stock indicators

--- lib/services/api_stocks_indicators.dart
+++ lib/services/api_stocks_indicators.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class StockIndicatorsApi {
-  final String apiUrl = "https://mfinance.com.br/api/v1/stocks/indicators";
+  // final String apiUrl = "https://mfinance.com.br/api/v1/stocks/indicators";
+  final String apiUrl = "https://mfinance.com.br/api/v1/stocks";
 
   Future<List<dynamic>> fetchStockIndicators() async {
     var response = await http.get(Uri.parse(apiUrl));
@@ -13,7 +14,7 @@ class StockIndicatorsApi {
 
       // If the call to the server was successful, parse the JSON
       var jsonData = json.decode(response.body);
-      var indicators = jsonData['indicators'];
+      var indicators = jsonData['stocks'];
       return indicators;
     } else {
       // If that call was not successful, throw an error.
