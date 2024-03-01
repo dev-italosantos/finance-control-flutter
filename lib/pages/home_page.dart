e0a6103aa5e97abefa98ae0ad6740fe2c0b15229
dev-italosantos <italotayllor@gmail.com>
2024-02-29 21:07:54 -0300

    refactor: modify section stocks add search

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,34 +20,42 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Active> selecionadas = [];
+  List<dynamic> filteredStocks =
+      []; // Adicione esta linha para declarar filteredStocks
+  String searchText =
+      ''; // Variável de estado para armazenar o texto de pesquisa
   NumberFormat real = NumberFormat.currency(locale: 'pt-br', name: 'R\$');
 
   StockIndicatorsApi api = StockIndicatorsApi();
   List<dynamic> stockIndicators = [];
-
 
   final PageController _controller = PageController();
   int _currentPage = 0;
   final List<Map<String, dynamic>> newsList = [
     {
       "title": "Bitcoin reaches new all-time high!",
-      "image": "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
+      "image":
+          "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
     },
     {
       "title": "Major companies now accepting Bitcoin as payment",
-      "image": "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
+      "image":
+          "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
     },
     {
       "title": "Bitcoin price analysis: Is it the right time to invest?",
-      "image": "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
+      "image":
+          "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
     },
     {
       "title": "Government regulations shake up the Bitcoin market",
-      "image": "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
+      "image":
+          "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
     },
     {
       "title": "Top 5 Bitcoin wallets for secure storage",
-      "image": "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
+      "image":
+          "https://www.cointribune.com/app/uploads/2024/02/bitcoin-proces-Australien-2.png",
     },
   ];
 
@@ -68,6 +76,7 @@ class _HomePageState extends State<HomePage> {
         curve: Curves.easeInOut,
       );
     });
+    // (stockIndicators);
     fetchData();
   }
 
@@ -80,6 +89,8 @@ class _HomePageState extends State<HomePage> {
 
   fetchData() async {
     var data = await api.fetchStockIndicators();
+
+    filteredStocks = List.from(data);
     setState(() {
       stockIndicators = data;
     });
@@ -89,7 +100,7 @@ class _HomePageState extends State<HomePage> {
     if (selecionadas.isEmpty) {
       return AppBar(
         title: Text(
-          'Home',
+          'worthy',
           style: TextStyle(
             fontSize: 18,
             color: Colors.white,
@@ -98,6 +109,16 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Colors.black,
         actions: [
+          IconButton(
+            icon: Icon(
+              Icons.favorite, // Ícone de notificação
+              color: Colors.white,
+              size: 20.0,
+            ),
+            onPressed: () {
+              // Adicione aqui a ação desejada para o ícone de notificação
+            },
+          ),
           IconButton(
             icon: Icon(
               Icons.notifications, // Ícone de notificação
@@ -125,7 +146,6 @@ class _HomePageState extends State<HomePage> {
       );
     }
   }
-
 
   showDetails(Active active) {
     Navigator.push(
@@ -161,7 +181,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -169,6 +188,7 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: SizedBox(
         height: 70.0,
         child: BottomAppBar(
+          color: Colors.grey[200],
           shape: const CircularNotchedRectangle(),
           child: Row(
             mainAxisSize: MainAxisSize.max,
@@ -187,15 +207,15 @@ class _HomePageState extends State<HomePage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: selecionadas.isNotEmpty
           ? Padding(
-        padding: const EdgeInsets.only(bottom: 16.0),
-        child: FloatingActionButton(
-          backgroundColor: Colors.grey,
-          onPressed: () {},
-          shape: const CircleBorder(),
-          elevation: 0.0,
-          child: const Icon(Icons.add, color: Colors.black),
-        ),
-      )
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: FloatingActionButton(
+                backgroundColor: Colors.grey,
+                onPressed: () {},
+                shape: const CircleBorder(),
+                elevation: 0.0,
+                child: const Icon(Icons.add, color: Colors.black),
+              ),
+            )
           : null,
       body: Column(
         children: [
@@ -206,7 +226,7 @@ class _HomePageState extends State<HomePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Padding(
-                  padding: EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0),
+                  padding: EdgeInsets.only(left: 16.0, top: 8.0),
                   child: Text(
                     'New',
                     style: TextStyle(
@@ -218,97 +238,134 @@ class _HomePageState extends State<HomePage> {
                 ),
                 SizedBox(
                   height: 200,
-                  child: stockIndicators.isEmpty // Verifica se os dados estão vazios
+                  child: stockIndicators
+                          .isEmpty // Verifica se os dados estão vazios
                       ? Shimmer.fromColors(
-                    baseColor: Colors.grey[300]!,
-                    highlightColor: Colors.grey[100]!,
-                    child: PageView.builder(
-                      controller: _controller,
-                      itemCount: 5, // Define um valor fixo para o shimmer
-                      itemBuilder: (_, __) => Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Card(
-                          elevation: 4,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16.0),
-                          ),
-                          child: Container(), // Container vazio para o efeito shimmer
-                        ),
-                      ),
-                    ),
-                  )
-                      : PageView.builder(
-                    controller: _controller,
-                    itemCount: newsList.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Card(
-                          elevation: 4,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16.0),
-                          ),
-                          child: Stack(
-                            children: [
-                              SizedBox(
-                                width: double.infinity,
-                                child: ClipRRect(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor: Colors.grey[100]!,
+                          child: PageView.builder(
+                            controller: _controller,
+                            itemCount: 5, // Define um valor fixo para o shimmer
+                            itemBuilder: (_, __) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Card(
+                                elevation: 4,
+                                shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(16.0),
-                                  child: Image.network(
-                                    newsList[index]['image'],
-                                    fit: BoxFit.cover,
-                                    errorBuilder: (context, error, stackTrace) {
-                                      return const Center(
-                                        child: Icon(Icons.error),
-                                      );
-                                    },
-                                  ),
                                 ),
+                                child:
+                                    Container(), // Container vazio para o efeito shimmer
                               ),
-                              Positioned(
-                                bottom: 0,
-                                left: 0,
-                                right: 0,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: const BorderRadius.only(
-                                      bottomLeft: Radius.circular(16.0),
-                                      bottomRight: Radius.circular(16.0),
-                                    ),
-                                    color: Colors.black.withOpacity(0.6),
-                                  ),
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    newsList[index]['title'],
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(fontSize: 16.0, color: Colors.white),
-                                  ),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
+                        )
+                      : PageView.builder(
+                          controller: _controller,
+                          itemCount: newsList.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Card(
+                                elevation: 4,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                                child: Stack(
+                                  children: [
+                                    SizedBox(
+                                      width: double.infinity,
+                                      child: ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(16.0),
+                                        child: Image.network(
+                                          newsList[index]['image'],
+                                          fit: BoxFit.cover,
+                                          errorBuilder:
+                                              (context, error, stackTrace) {
+                                            return const Center(
+                                              child: Icon(Icons.error),
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.only(
+                                            bottomLeft: Radius.circular(16.0),
+                                            bottomRight: Radius.circular(16.0),
+                                          ),
+                                          color: Colors.black.withOpacity(0.6),
+                                        ),
+                                        padding: EdgeInsets.all(8.0),
+                                        child: Text(
+                                          newsList[index]['title'],
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.white),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
                         ),
-                      );
-                    },
-                  ),
                 ),
               ],
             ),
           ),
-          // Seção de Indicadores de Ações
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0),
-                  child: const Text(
+                const Padding(
+                  padding: EdgeInsets.only(left: 16.0, top: 8.0),
+                  child: Text(
                     'Stocks',
                     style: TextStyle(
                       fontSize: 18.0,
                       fontWeight: FontWeight.bold,
                       color: Colors.black,
                     ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          onChanged: (value) {
+                            setState(() {
+                              filteredStocks = stockIndicators
+                                  .where((stock) => stock['symbol']
+                                      .toLowerCase()
+                                      .contains(value.toLowerCase()))
+                                  .toList();
+                            });
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'search',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      Icon(Icons.search, color: Colors.grey),
+                    ],
                   ),
                 ),
                 Expanded(
@@ -336,7 +393,8 @@ class _HomePageState extends State<HomePage> {
                         // Exibe o conteúdo após o carregamento
                         return ListView.separated(
                           itemBuilder: (BuildContext context, int active) {
-                            bool isSelected = selecionadas.contains(stockIndicators[active]);
+                            bool isSelected =
+                                selecionadas.contains(filteredStocks[active]);
 
                             return ListTile(
                               shape: const RoundedRectangleBorder(
@@ -346,14 +404,14 @@ class _HomePageState extends State<HomePage> {
                               ),
                               leading: isSelected
                                   ? const CircleAvatar(
-                                child: Icon(Icons.check),
-                              )
+                                      child: Icon(Icons.check),
+                                    )
                                   : SizedBox(
-                                width: 40.0,
-                                child: Image.asset(AppIcons.btc),
-                              ),
+                                      width: 40.0,
+                                      child: Image.asset(AppIcons.btc),
+                                    ),
                               title: Text(
-                                stockIndicators[active]['symbol'],
+                                filteredStocks[active]['symbol'],
                                 style: const TextStyle(
                                   color: Colors.black54,
                                   fontSize: 17.0,
@@ -361,23 +419,26 @@ class _HomePageState extends State<HomePage> {
                                 ),
                               ),
                               trailing: Text(
-                                real.format(stockIndicators[active]['lastPrice']),
+                                real.format(
+                                    filteredStocks[active]['lastPrice']),
                               ),
                               selected: isSelected,
                               tileColor: isSelected ? Colors.red : null,
                               onLongPress: () {
                                 setState(() {
                                   isSelected
-                                      ? selecionadas.remove(stockIndicators[active])
-                                      : selecionadas.add(stockIndicators[active]);
+                                      ? selecionadas
+                                          .remove(filteredStocks[active])
+                                      : selecionadas
+                                          .add(filteredStocks[active]);
                                 });
                               },
-                              onTap: () => showDetails(stockIndicators[active]),
+                              onTap: () => showDetails(filteredStocks[active]),
                             );
                           },
                           padding: const EdgeInsets.all(16.0),
                           separatorBuilder: (_, __) => const Divider(),
-                          itemCount: stockIndicators.length,
+                          itemCount: filteredStocks.length,
                         );
                       }
                     },
@@ -393,14 +454,15 @@ class _HomePageState extends State<HomePage> {
 
   Widget _bottomAction(IconData icon, VoidCallback onTap) {
     return InkWell(
-        onTap: onTap,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Icon(
-            icon,
-            color: Colors.grey[900],
-            size: 20.0,
-          ),
-        ));
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(
+          icon,
+          color: Colors.grey[900],
+          size: 20.0,
+        ),
+      ),
+    );
   }
 }
