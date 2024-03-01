025b69ca02d9f7accdea55fc9a73a9fdd5361674
dev-italosantos <italotayllor@gmail.com>
2024-02-29 22:11:25 -0300

    refactor: comment temporarily

--- lib/repositories/active_repository.dart
+++ lib/repositories/active_repository.dart
@@ -1,37 +1,37 @@
-import 'package:flutter_investment_control/core/app_icons.dart';
-import 'package:flutter_investment_control/models/active_model.dart';
-
-class ActiveRepository {
-  static List<Active> tabela = [
-    Active(
-      icon: AppIcons.btc,
-      name: 'Bitcoin',
-      acronym: 'BTC',
-      price: 22777,
-    ),
-    Active(
-      icon: AppIcons.btc,
-      name: 'Ethereum',
-      acronym: 'ETH',
-      price: 2095,
-    ),
-    Active(
-      icon: AppIcons.btc,
-      name: 'Polygon',
-      acronym: 'MATIC',
-      price: 0.807449,
-    ),
-    Active(
-      icon: AppIcons.btc,
-      name: 'Solana',
-      acronym: 'SOL',
-      price: 61.75,
-    ),
-    Active(
-      icon: AppIcons.btc,
-      name: 'Chainlink',
-      acronym: 'LINK',
-      price: 15.30,
-    )
-  ];
-}
+// import 'package:flutter_investment_control/core/app_icons.dart';
+// import 'package:flutter_investment_control/models/active_model.dart';
+//
+// class ActiveRepository {
+//   static List<Active> tabela = [
+//     Active(
+//       icon: AppIcons.btc,
+//       name: 'Bitcoin',
+//       symbol: 'BTC',
+//       lastPrice: 22777,
+//     ),
+//     Active(
+//       icon: AppIcons.btc,
+//       name: 'Ethereum',
+//       symbol: 'ETH',
+//       lastPrice: 2095,
+//     ),
+//     Active(
+//       icon: AppIcons.btc,
+//       name: 'Polygon',
+//       symbol: 'MATIC',
+//       lastPrice: 0.807449,
+//     ),
+//     Active(
+//       icon: AppIcons.btc,
+//       name: 'Solana',
+//       symbol: 'SOL',
+//       lastPrice: 61.75,
+//     ),
+//     Active(
+//       icon: AppIcons.btc,
+//       name: 'Chainlink',
+//       symbol: 'LINK',
+//       lastPrice: 15.30,
+//     )
+//   ];
+// }
