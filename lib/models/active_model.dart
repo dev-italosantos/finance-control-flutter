0d30049133fe2bbaa54a0d7da087f83922a246c4
dev-italosantos <italotayllor@gmail.com>
2024-03-06 14:20:07 -0300

    refactor: refactor: modify active model add properties pt.2

--- lib/models/active_model.dart
+++ lib/models/active_model.dart
@@ -2,12 +2,22 @@ class Active {
   String icon;
   String name;
   String symbol;
+  String sector;
+  String segment;
   double lastPrice;
+  double dividendYield;
+  double lastYearLow;
+  double lastYearHigh;
 
   Active({
     required this.icon,
     required this.name,
     required this.symbol,
-    required this.lastPrice
+    required this.sector,
+    required this.segment,
+    required this.lastPrice,
+    required this.dividendYield,
+    required this.lastYearLow,
+    required this.lastYearHigh
   });
 }
