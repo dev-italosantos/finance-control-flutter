422902d17a66438823be90ed366fe8d655de32be
dev-italosantos <italotayllor@gmail.com>
2024-03-07 15:08:39 -0300

    refactor: add map string for alignment with future build

--- lib/services/api_stocks_historicals.dart
+++ lib/services/api_stocks_historicals.dart
@@ -5,19 +5,19 @@ import 'package:http/http.dart' as http;
 class StocksHistoricals {
   Future<Map<String, dynamic>?> getStockHistoricals(String ticker) async {
     final String apiUrl =
-        "https://mfinance.com.br/api/v1/stocks/historicals/$ticker?months=1";
+        "https://mfinance.com.br/api/v1/stocks/historicals/$ticker?months=12";
 
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
+    final response = await http.get(Uri.parse(apiUrl));
 
-      if (response.statusCode == 200) {
-        final jsonData = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      print('Data received: ${response.body}');
 
-        var historicals = jsonData['historicals'];
-        return historicals;
-      } else {
-        throw Exception('Failed to load stock indicators');
-      }
-    } catch (err) {}
+      final jsonData = jsonDecode(response.body) as Map<String, dynamic>;
+
+      return jsonData;
+    } else {
+      throw Exception('Failed to load stock indicators');
+    }
   }
 }
+
